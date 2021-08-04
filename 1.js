e463ed3ac8e75d9f530aa3b9df2821e1d2d3d536
daryllmamaril <dazzmamaril@gmail.com>
2021-08-04 18:40:28 +0800

    onlyDuplicates Problem Completed

---
+++ 1.js
@@ -0,0 +1,9 @@
+// Given a string, remove any characters that are unique from the string.
+
+function onlyDuplicates(str) {
+  return str
+    .split("")
+    .filter((char) => str.indexOf(char) !== str.lastIndexOf(char))
+    .join("");
+}
+console.log(onlyDuplicates("abccdefee")); //-->"cceee";
