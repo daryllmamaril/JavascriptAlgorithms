2e40d6ee0d76a50f063adad30c73f5b167e2bb0a
daryllmamaril <dazzmamaril@gmail.com>
2021-10-06 12:15:23 +0800

    Sliding Window Pattern maxSubArraySum naive solution done

---
+++ patterns/slidingWindow/maxSubArraySum.js
@@ -0,0 +1,20 @@
+// SLIDING WINDOW PATTERN
+
+//NAIVE SOLUTION (Time Complexity O(N^2))
+function maxSubArraySum1(array, num) {
+  if (array.length < num) {
+    return null;
+  }
+  let max = -Infinity;
+  for (let i = 0; i < array.length - num + 1; i++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += array[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
+console.log(maxSubArraySum1([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)); //19
