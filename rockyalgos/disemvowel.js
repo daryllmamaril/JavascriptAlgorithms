ef9b4fca3f5d58d944f9bf49fbd83d3371276e13
daryllmamaril <dazzmamaril@gmail.com>
2022-11-18 12:39:46 +0800

    modified disemvowel.js

--- rockyalgos/disemvowel.js
+++ rockyalgos/disemvowel.js
@@ -4,8 +4,18 @@
 // [] tells to match any character in a set.
 // aeiou are the characters in the set.
 // " means replace aeiou with an empty string"
-
 function disemvowel(str) {
   return str.replace(/[aeiou]/gi, '');
 }
 console.log(disemvowel("Ima take off all the VOWELS in this sentence."))
+
+function disemvowel2(str) {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let result = "";
+  str.split("").map(char => {
+    if (!vowels.includes(char)) {
+       result += char;
+    }
+  })
+  return result;
+}
