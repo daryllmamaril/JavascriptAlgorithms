b3534a5874736618f8e02ee61cd409776cd6ace3
daryllmamaril <dazzmamaril@gmail.com>
2023-02-20 13:49:28 +0800

    Reviewed and solved again findParityOutlier and findTheOddInt

--- codewars/findParityOutlier.js
+++ codewars/findParityOutlier.js
@@ -1,4 +1,4 @@
-// You are given an array (which will have num length of at least 3, but could be very large) containing numegers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+// You are given an array (which will have num length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
 function findOutlier(integers) {
   const evens = [];
@@ -16,3 +16,4 @@ function findOutlier2(integers) {
   return evens.length === 1 ? evens[0] : odds[0];
 }
 console.log(findOutlier2([160, 3, 1719, 19, 11, 13, -21])); //Should return: 160 (the only even number)
+
