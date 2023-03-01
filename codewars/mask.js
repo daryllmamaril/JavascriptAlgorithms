07bf44689e4e0ac41ca687abe1ab5aedb2968925
daryllmamaril <dazzmamaril@gmail.com>
2023-03-01 15:54:13 +0800

    Redid mask.js for toy problems review purposes

--- codewars/mask.js
+++ codewars/mask.js
@@ -2,7 +2,7 @@
 
 function mask(string) {
   return string.slice(-4).padStart(string.length, "#");
-}
+};
 console.log(mask("123456789")); //"#####6789"
 console.log(mask("00000000001771"));
 
@@ -12,6 +12,7 @@ console.log(mask("00000000001771"));
 //
 //
 //
+
 
 const mask2 = (string) => string.slice(-4).padStart(string.length, "#");
 console.log(mask2("12345")); //"#2345"
