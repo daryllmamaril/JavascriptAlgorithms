4dd83d424e521bddb3bec44b743dc270faaf109e
daryllmamaril <dazzmamaril@gmail.com>
2023-03-07 23:15:57 +0800

    redid onlyDuplicates.js for review purposes

--- codewars/onlyDuplicates.js
+++ codewars/onlyDuplicates.js
@@ -1,9 +1,10 @@
 // Given a string, remove any characters that are unique from the string.
 
-function onlyDuplicates(str) {
+function onlyDuplicates1(str) {
   return str
     .split("")
     .filter((char) => str.indexOf(char) !== str.lastIndexOf(char))
     .join("");
 }
 console.log(onlyDuplicates("abccdefee")); //-->"cceee";
+console.log(onlyDuplicates("dddaz")); //-->"ddd";
